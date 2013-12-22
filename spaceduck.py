8aa0c45c5a9a89b03670d9a04cdd59d8cda776de
Ben Nied <spacewreckage@gmail.com>
2013-12-22 18:18:57 -0500

    first real commit

---
+++ spaceduck.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+'''
+spaceduck.org site
+
+Let's make a site!!! yay!!
+'''
+
+import cherrypy
+from os.path import dirname, abspath, sep
+from Cheetah.Template import Template
+
+# Globals
+current_dir = dirname(abspath(__file__)) + sep
+
+class Webpage(object):
+	def index(self):
+		htmlString = "<html>\n\t<head>\n\t\t<title>spaceduck.org</title>\n\t</head>\n\t<body>\n\t\tTesting 1 2 3\n\t</body>\n</html>"
+
+		return htmlString
+
+	index.exposed = True
+
+class run():
+	cherrypy.quickstart(WebPage(), '/', config)
+	cherrypy.engine.block()
+
+# Config
+config = {
+	'global': {
+		'server.socket_host': '0.0.0.0',
+		'server.socket_port': 9000,
+	}, 
+	'/': {
+		'tools.staticdir.root': current_dir,
+	},
+}
